5083996cef19ae7d592e99b2f941a72c746c8e84
Jae-Seung Lee <jaeseung@gmail.com>
2017-06-25 14:44:54 -0400

    Added outlets for topTextField and bottomTextField. Set up the text attributes in viewDidLoad()

--- MemeMe/MemeMeViewController.swift
+++ MemeMe/MemeMeViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class MemeMeViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    // MARK: Properties
+    let memeTextAttribute: [String: Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: Float(2.0)]
+    
+    
+    // MARK: Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topTextField.defaultTextAttributes = memeTextAttribute
+        bottomTextField.defaultTextAttributes = memeTextAttribute
+        
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
     }
 
     override func didReceiveMemoryWarning() {
