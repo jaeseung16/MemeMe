e9c89a8a5c76792f7b982eab162bae0673c04867
Jae-Seung Lee <jaeseung@gmail.com>
2017-06-29 18:47:56 -0400

    Added Meme struct

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Jae-Seung Lee on 6/29/17.
+//  Copyright © 2017 Jae-Seung Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    var memedImage: UIImage
+    
+}
